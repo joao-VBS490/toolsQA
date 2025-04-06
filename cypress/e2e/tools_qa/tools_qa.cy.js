81d6a4a3a173476f95475078dd50b7ff6cfb9e5b
9UJAO6 <jvictorbs.03@gmail.com>
2025-04-06 00:38:39 -0300

    testando checkbox e arvore recursiva

--- cypress/e2e/tools_qa/tools_qa.cy.js
+++ cypress/e2e/tools_qa/tools_qa.cy.js
@@ -22,7 +22,7 @@ describe('automação de testes com toolsQA', ()=>{
         
     });
 
-    it.only ('teste do card elements', () => {
+    it ('teste do card elements - textbox', () => {
         cy.contains('.card-body', 'Elements').click();
         cy.url().should('include', '/elements');
         cy.get('#item-0').should('have.text','Text Box').click();
@@ -31,7 +31,61 @@ describe('automação de testes com toolsQA', ()=>{
         cy.get('#userEmail').focus().type('joaoteste@teste.com');
         cy.get('#currentAddress').focus().type('Rua cypress');
         cy.get('#permanentAddress').focus().type('Rua cypress');
-    })
+        cy.get('#submit'). click();
+        cy.get('#output').should('be.visible');
+        cy.get('.mb-1').each(($el) => {
+            const cadastro = $el.text();
+            cy.log(cadastro);
+        });
+        cy.get('#app header a img[src="/images/Toolsqa.jpg"]').click();
+    });
+
+    it.only('testando card elements - checkbox', ()=> {
+        cy.contains('.card-body', 'Elements').click();
+        cy.url().should('include', '/elements');
+        cy.get('#item-1').should('have.text','Check Box').click();
+        cy.get('.text-center').should('have.text','Check Box');
+        cy.get('.rct-checkbox').click();
+        cy.get('#result').should('be.visible').then(($el)=> { // verifica o texto retornado após clicar no checkbox e printa no log
+            cy.log($el.text());
+        });
+        cy.get('.rct-checkbox').click();
+        cy.get('button[aria-label="Toggle"]').click();
+        cy.get('.rct-title').each(($el) => {
+            const homechild = $el.text();
+            cy.log(homechild);            
+//captura o texto da opção desktop clica no checkbox e verifica o texto gerado//
+            if (homechild.includes('Desktop')) {
+                cy.wrap($el).should('be.visible').click();
+                cy.get('#result');
+                cy.get ('button[aria-label="Toggle"]').eq(1).click();
+                cy.contains('.rct-title', 'Notes'). should('be.visible');
+                cy.contains('.rct-title', 'Commands'). should('be.visible');
+                cy.get ('button[aria-label="Toggle"]').eq(1).click();
+            };
+//captura o texto da opção Documents clica no checkbox e verifica o texto gerado//            
+            if (homechild.includes('Documents')) {
+                cy.wrap($el).should('be.visible').click();
+                cy.get('#result');
+                cy.get ('button[aria-label="Toggle"]').eq(2).click();
+                cy.contains('.rct-title', 'WorkSpace'). should('be.visible');
+                cy.contains('.rct-title', 'Office'). should('be.visible');
+                cy.get ('button[aria-label="Toggle"]').eq(2).click();
+            };
+//captura o texto da opção Downloads clica no checkbox e verifica o texto gerado//
+            if (homechild.includes('Downloads')) {
+                cy.wrap($el).should('be.visible').click();
+                cy.get('#result');
+                cy.get ('button[aria-label="Toggle"]').eq(3).click();
+                cy.contains('.rct-title', 'Word File.doc'). should('be.visible');
+                cy.contains('.rct-title', 'Excel File.doc'). should('be.visible');
+                cy.get ('button[aria-label="Toggle"]').eq(3).click();
+        
+            }
+        });    
+    });
+    
+
 });
 
 
