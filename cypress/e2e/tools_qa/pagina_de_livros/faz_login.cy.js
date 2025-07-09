2f3f883e9fe45349851a07fc933702cffd00329a
João Santana <joao.barbosa@crremocao.com.br>
2025-07-09 13:30:40 -0300

    validação da pagina de login

--- cypress/e2e/tools_qa/pagina_de_livros/faz_login.cy.js
+++ cypress/e2e/tools_qa/pagina_de_livros/faz_login.cy.js
@@ -12,35 +12,33 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   }
 });
 
+describe('Adicionar livros', () => {
+  beforeEach(() => {
+    cy.viewport(1366, 780);
+    cy.visit('https://demoqa.com/books'); // Acessa a URL
+    cy.get('#app header a img[src="/images/Toolsqa.jpg"]').should('be.visible');
+  });
 
-describe('fazendo login na pagina de livros do ToolsQA', () => {
-    beforeEach(() => {
-        cy.viewport(1366, 780);
-        cy.visit('https://demoqa.com/'); // Acessa a URL
-        cy.get('#app header a img[src="/images/Toolsqa.jpg"]').should('be.visible');
-        cy.fixture('fakeUser.json').as('fakeUser');
-        
-        
-    })
-    it('faz login', function (){
-
-        cy.get('@fakeUser').then((user) => {
-            cy.get('.card-body')
-                .contains('Book Store Application')
-                .click();
-            cy.url().should('include', '/books');
-            cy.get('.header-text')
-                .should('contain', 'Book Store Application');
-            cy.get('#item-0')
-                .get(':nth-child(6) > .element-list > .menu-list > #item-0')
-                .click();
-            cy.get('.text-center')
-                .should('be.visible')
-                .and('have.text', 'Login');
-            cy.get('#userName').type(this.fakeUser.username);
-            cy.get('#password').type(this.fakeUser.password);
-            cy.get('#login').click();
-            
-        });
+  it('adiciona um livro', ()=> {
+    cy.fixture('fakeUser.json').then((userData) => {
+      cy.get('.books-wrapper').then(()=> {
+        console.log('sucesso, estamos na pagina de livros');
+      });
+      cy.get('#login')
+        .click();
+      cy.url().should('include', '/login');
+      cy.get('.login-wrapper').then(() => {
+        console.log('sucesso, redirecionado para a tela de login');
+      });
+      cy.get('#userName')
+        .type(userData.username);
+      cy.get('#password')
+        .type(userData.password);
+      cy.get('#login')
+        .click();
+      cy.get('#userName-value')
+        .should('be.visible')
+        .contains(userData.username);
     });
-})
+  });
+});
